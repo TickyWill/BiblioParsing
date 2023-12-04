e1e133c944b88c31f75b35c5b170e2e3629d2cf1
Bertin-fap <francois.bertin7@wanadoo.fr>
2023-12-04 13:41:38 +0100

    global cleaning

--- BiblioParsing/BiblioSpecificGlobals.py
+++ BiblioParsing/BiblioSpecificGlobals.py
@@ -14,54 +14,37 @@ __all__ = ['BASIC_KEEPING_WORDS',
            'CONCATENATED_XLSX',
            'COOC_AUTHORIZED_ITEMS',
            'COOC_AUTHORIZED_ITEMS_DICT',
-           'COOC_COLOR_NODES',
-           'COOC_NETWORKS_FILE',
-           'COOC_HTML_PARAM',
            'COUNTRY_TOWNS',
-           'COUPL_AUTHORIZED_ITEMS',
-           'COUPL_FILENAME_GEXF',
-           'COUPL_FILENAME_XLSX',
-           'COUPL_GLOBAL_VALUES',
-           'COUPL_HTML_PARAM',
            'DEDUPLICATED_XLSX',
            'DIC_DOCTYPE',
            'DIC_AMB_WORDS',
            'DIC_LOW_WORDS',
-           'DIC_OUTDIR_DESCRIPTION',
            'DIC_OUTDIR_PARSING',
            'DIC_INST_FILENAME',
            'DIC_TOWN_SYMBOLS',
            'DIC_TOWN_WORDS',
            'DIC_WORD_RE_PATTERN',
-           'DISTRIBS_ITEM_FILE',
            'DROPING_WORDS',
            'DROPING_SUFFIX',
            'EMPTY',
            'ENCODING',
            'FIELD_SIZE_LIMIT',
-           'FOLDER_NAMES',
            'FOLDER_SELECTION_HELP_TEXT',
            'FR_DROPING_WORDS',
            'GEN_KEEPING_WORDS',
            'GUI_BUTTON_RATIO',
            'GUI_TEXT_MAX_LINES_NB',
            'GUI_WIDGET_RATIO',
-           'HEADER',
            'INST_BASE_LIST',
            'INST_FILTER_LIST',
            'KEEPING_WORDS',
            'KEEPING_PREFIX',
            'LABEL_MEANING',
-           'LENGTH_DIFF_THRESHOLD',
            'LENGTH_THRESHOLD',
            'MISSING_SPACE_ACRONYMS',
-           'NAME_MEANING',
            'NLTK_VALID_TAG_LIST',
-           'NMAX_NODES',
            'NORM_JOURNAL_COLUMN_LABEL',
            'NOUN_MINIMUM_OCCURRENCES',
-           'NODE_SIZE_REF',
-           'PARSING_PERF',
            'RAW_INST_FILENAME',
            'RE_ADDRESS',
            'RE_ADDS_JOURNAL',
@@ -91,14 +74,12 @@ __all__ = ['BASIC_KEEPING_WORDS',
            'SCOPUS_CAT_CODES',
            'SCOPUS_JOURNALS_ISSN_CAT',
            'SIMILARITY_THRESHOLD',
-           'SIZE_MIN',
            'SMALL_WORDS_DROP',
            'SYMBOL',
            'UNKNOWN',
            'USECOLS_SCOPUS',
            'USECOLS_WOS',
            'USER_KEEPING_WORDS',
-           'VALID_LABEL_GRAPH',
            'WOS',
           ]
 
@@ -274,70 +255,6 @@ INST_TYPES_USECOLS = ['Level', 'Abbreviation']
 COOC_AUTHORIZED_ITEMS = ['AU','CU','AK','IK','TK','S','S2']
 
 
-COOC_COLOR_NODES = {"Y" : "255,255,0",  # default color for gephi display
-                    "J" : "150,0,150",
-                    "AU": "20,50,255",
-                    "IK": "255,0,255",
-                    "AK": "255,0,255",
-                    "TK": "205,0,205",
-                    "S" : "50,0,150",
-                    "S2": "50,0,150",
-                    "R" : "255,0,0",
-                    "RJ": "255,97,0",
-                    "I" : "0,255,0",
-                    "CU": "0,255,255",
-                    "LA": "0,180,0",
-                    "DT": "0,180,0",
-                   }
-
-
-COOC_HTML_PARAM = {'algo'      : 'barnes',
-                   'height'    : 1000,
-                   'width'     : 1000,
-                   'bgcolor'   : '#EAEDED',
-                   'font_color': 'black',
-                  }
-
-
-COOC_NETWORKS_FILE = 'coocnetworks.json'
-
-
-COUPL_AUTHORIZED_ITEMS = ['AU','CU','I','AK','IK','TK','S','S2']
-
-
-COUPL_FILENAME_XLSX = 'biblio_network.xlsx'
-
-
-COUPL_FILENAME_GEXF = 'biblio_network.gexf'
-
-
-COUPL_GLOBAL_VALUES = {'BCTHR' :  1,  # minimum number of shared references 
-                                      # for keeping an edge
-                       'RTUTHR': 2,   # minimum times of use in the corpus
-                                      # to count a reference in the 
-                                      # shared references
-                       'WTHR'  :   0, # minimum weight to keep a link
-                       'NRTHR' :  1,  # minimum number of references to keep a node
-                       }
-
-
-COUPL_HTML_PARAM = {'background_color': '#EAEDED', #light grey,
-                    'font_color'      : 'black',
-                    'edges_color'     : '#808080',       # gray,
-                    'nodes_colors'    : {0        : '#5677fc',  #blue
-                                         1        : '#3f51b5',  #indigo
-                                         2        : '#e51c23',  #red
-                                         3        : '#00bcd4',  #cyan
-                                         4        : '#259b24',  #green
-                                         5        : '#ffeb3b',  #yellow
-                                         6        : '#ff9800',  #orange
-                                         7        : '#795548',  #brown 
-                                         8        : '#cddc39',  #lime   # limited number of colored values 
-                                         'uncolor': '#e0e0e0',  # grey 300 - for nodes out of colored values
-                                     },
-                    }
-
-
 DEDUPLICATED_XLSX = 'articles_dedup.xlsx'
 
 
@@ -409,9 +326,6 @@ DIC_TOWN_WORDS = {" lez " : " les ",
                  } 
 
 
-DISTRIBS_ITEM_FILE = 'DISTRIBS_itemuse.json'
-
-
 EMPTY = 'empty'
 
 
@@ -419,20 +333,6 @@ ENCODING = 'iso-8859-1' # encoding used by the function read_database_wos
 
 
 FIELD_SIZE_LIMIT = 256<<10 # extend maximum field size for wos file reading
-
-
-FOLDER_NAMES = {'corpus'      : 'Corpus',
-                'concat'      : 'concatenation',
-                'cooccurrence': 'cooc',
-                'coupling'    : 'coupling',
-                'description' : 'freq',
-                'dedup'       : 'deduplication',
-                'filtering'   : 'filter',
-                'parsing'     : 'parsing',
-                'rawdata'     : 'rawdata',
-                'scopus'      : 'scopus',
-                'wos'         : 'wos',
-               }
 
 
 FOLDER_SELECTION_HELP_TEXT ='''The selected folder is edited.
@@ -446,10 +346,7 @@ GUI_BUTTON_RATIO = 2.5
 GUI_TEXT_MAX_LINES_NB = 3
 
 
-GUI_WIDGET_RATIO = 1.2 
-
- 
-HEADER = True                                                                                      #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+GUI_WIDGET_RATIO = 1.2                                                                                    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 LABEL_MEANING = {'AU':'Authors',              # ex: Nom1 J, Nom2 E, Nom3 J-P
@@ -469,44 +366,15 @@ LABEL_MEANING = {'AU':'Authors',              # ex: Nom1 J, Nom2 E, Nom3 J-P
                 } 
 
 
-LENGTH_DIFF_THRESHOLD = 10                # unused
-
-
 LENGTH_THRESHOLD = 30
-
-
-NAME_MEANING = {'years'          : 'Y',
-                'languages'      : 'LA',
-                'doctypes'       : 'DT',
-                'countries'      : 'CU',
-                'institutions'   : 'I',
-                'journals'       : 'J',
-                'references'     : 'R',
-                'refjournals'    : 'RJ',
-                'subjects'       : 'S',
-                'subjects2'      : 'S2',
-                'journalkeywords': 'IK',
-                'titlekeywords'  : 'TK',
-                'authorskeywords': 'AK',
-                'authors'        :'AU',
-               }
 
 
 NLTK_VALID_TAG_LIST = ['NN','NNS','VBG','JJ'] # you can find help on the nltk tags set
                                               # using nltk.help.upenn_tagset() 
-    
-    
-NMAX_NODES = 100 # maximum number of nodes to keep in corpus description 
 
 
 NOUN_MINIMUM_OCCURRENCES = 3 # Minimum occurrences of a noun to be retained when 
                              # building the set of title keywords see build_title_keywords
-    
-    
-NODE_SIZE_REF = 30
-
-
-PARSING_PERF = 'failed.json'
 
 
 RE_ADDRESS = re.compile('''(?<=\]\s)               # Captures: "xxxxx" in string between "]" and "["  
@@ -605,18 +473,10 @@ SCOPUS_JOURNALS_ISSN_CAT = 'scopus_journals_issn_cat.txt'
 
 SIMILARITY_THRESHOLD = 80
 
-
-SIZE_MIN = 1 # Minimum size of co-occurrence nodes
-
-
 SYMBOL = '\s,;:.\-\/'
 
 
 UNKNOWN = 'unknown'
-
-
-# Cooccurrence graph built only for the following labels
-VALID_LABEL_GRAPH = ['AU', 'CU', 'S', 'S2', 'IK', 'R', 'RJ', 'I', 'AK', 'TK'] 
 
 
 # WOS database name
@@ -632,17 +492,6 @@ COUNTRY_TOWNS = read_towns_per_country(COUNTRY_TOWNS_FILE, REP_UTILS, DIC_TOWN_S
 
 COOC_AUTHORIZED_ITEMS_DICT = {label:name for name,label in LABEL_MEANING.items() 
                               if name in COOC_AUTHORIZED_ITEMS}
-
-
-_DIC_OUTDIR_DESCRIPTION = {acronym:'freq_'+file for acronym,file in DIC_OUTDIR_PARSING.items()}
-
-_DIC_OUTDIR_DESCRIPTION_ADD = {'DT' : 'freq_doctypes.dat',
-                               'J'  : 'freq_journals.dat',
-                               'LA' : 'freq_languages.dat',
-                               'RJ' : 'freq_refjournals.dat',
-                               'Y'  : 'freq_years.dat',
-                              }
-DIC_OUTDIR_DESCRIPTION = res = {**_DIC_OUTDIR_DESCRIPTION, **_DIC_OUTDIR_DESCRIPTION_ADD}
 
 
 # This global is used in merge_database function
